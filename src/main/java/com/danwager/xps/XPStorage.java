5137957a202f89094404e8ea9fe1b9eb3560f926
Dan Wager <danielwager@gmail.com>
2017-02-09 21:20:28 -0800

    Remove commands from plugin

--- src/main/java/com/danwager/xps/XPStorage.java
+++ src/main/java/com/danwager/xps/XPStorage.java
@@ -1,9 +1,6 @@
 package com.danwager.xps;
 
 import com.danwager.xps.storage.StorageItemManager;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XPStorage extends JavaPlugin {
@@ -11,15 +8,5 @@ public class XPStorage extends JavaPlugin {
     @Override
     public void onEnable() {
         new StorageItemManager(this);
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player)sender;
-
-        player.sendMessage("xp: " + player.getExp());
-        player.sendMessage("level: " + player.getLevel());
-
-        return true;
     }
 }
